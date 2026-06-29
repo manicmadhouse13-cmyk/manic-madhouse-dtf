42e45052de08a147065687eca169325a05555474
manic-madhouse-dtf <manicmadhouse13@gmail.com>
2026-06-29 15:56:47 +1000

    Implement product display and add to cart feature
    Add product details and cart functionality to Product.js

--- Js/Product.js
+++ Js/Product.js
@@ -1,1 +1,37 @@
+const params = new URLSearchParams(window.location.search);
 
+const id = parseInt(params.get("id"));
+
+const product = products.find(p => p.id === id);
+
+document.getElementById("productName").innerText = product.name;
+
+document.getElementById("productPrice").innerText = "$" + product.price;
+
+document.getElementById("productImage").src = product.image;
+
+document.getElementById("addCart").onclick = () => {
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push({
+
+        id: product.id,
+
+        name: product.name,
+
+        image: product.image,
+
+        price: product.price,
+
+        qty: parseInt(document.getElementById("qty").value),
+
+        size: document.getElementById("size").value
+
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert(product.name + " added to cart!");
+
+};
