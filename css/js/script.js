1af4cf5d5bf01aea3b7ec01c563e72b3c0a040bf
manic-madhouse-dtf <manicmadhouse13@gmail.com>
2026-07-01 20:58:57 +1000

    Add image upload and preview functionality

--- css/js/script.js
+++ css/js/script.js
@@ -1,1 +1,37 @@
+const upload = document.getElementById("uploadImage");
+const preview = document.getElementById("designPreview");
 
+const sizeSlider = document.getElementById("sizeSlider");
+const rotateSlider = document.getElementById("rotateSlider");
+
+upload.addEventListener("change", function(){
+
+const file = this.files[0];
+
+if(!file) return;
+
+const reader = new FileReader();
+
+reader.onload = function(e){
+
+preview.src = e.target.result;
+preview.style.display = "block";
+
+}
+
+reader.readAsDataURL(file);
+
+});
+
+sizeSlider.addEventListener("input",function(){
+
+preview.style.width = this.value + "px";
+
+});
+
+rotateSlider.addEventListener("input",function(){
+
+preview.style.transform =
+`translate(-50%,-50%) rotate(${this.value}deg)`;
+
+});
