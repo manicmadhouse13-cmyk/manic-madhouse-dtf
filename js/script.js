b2d80a0a029b5c7612c99148ed50c097a4d4dabb
manic-madhouse-dtf <manicmadhouse13@gmail.com>
2026-07-12 07:30:42 +1000

    Add initialization functions on DOMContentLoaded

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+/*==================================================
+MANIC MADHOUSE DTF DESIGNS
+SCRIPT.JS
+VERSION 6.1
+==================================================*/
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    initialiseNavigation();
+    initialiseShop();
+    initialiseQuote();
+    initialiseDesignBuilder();
+    initialiseAnimations();
+
+});
