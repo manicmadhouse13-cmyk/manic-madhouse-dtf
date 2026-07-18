58381a7e4d810cd0f0dbfef40f87b7fcb34dc438
manic-madhouse-dtf <manicmadhouse13@gmail.com>
2026-07-18 22:25:23 +1000

    Refactor design-builder.js to improve HTML structure

--- js/design-builder.js
+++ js/design-builder.js
@@ -760,14 +760,35 @@ function updateQuoteBasketDisplay() {
                     </strong>
 
 
-                    <span>
-                    Quantity: ${item.quantity}
-                    </span>
+                    
+<span>
+Quantity: ${item.quantity}
+</span>
 
 
-                    <span>
-                    Size: ${item.size}px
-                    </span>
+<span>
+Size: ${item.size}px
+</span>
+
+
+<span>
+Shirt Colour: ${item.shirtColour}
+</span>
+
+
+<span>
+Shirt Size: ${item.shirtSize}
+</span>
+
+
+<span>
+Print Location: ${item.printLocation}
+</span>
+
+
+<span>
+Notes: ${item.notes || "None"}
+</span>
 
 
                 </div>
