11e3ea0660ece4796b29c44ba9db8774d49299e6
manic-madhouse-dtf <manicmadhouse13@gmail.com>
2026-07-23 22:37:14 +1000

    Update Supabase connection URL and key

--- js/supabase.js
+++ js/supabase.js
@@ -4,11 +4,11 @@ SUPABASE CONNECTION
 VERSION 2.0
 ==================================================*/
 
-const SUPABASE_URL = "https://ymkmpsgossabyznwhluk.supabase.co";
+const SUPABASE_URL =
+"https://ymkmpsgossabyznwhluk.supabase.co";
 
 const SUPABASE_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlta21wc2dvc3NhYnl6bndobHVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NDE0MTEsImV4cCI6MjEwMDExNzQxMX0.5YLtkFTon-5Qkm_hIBwcQb-0JZFCTyNw1QnlAzerxAg";
-
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlta21wc2dvc3NhYnl6bndobHVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NDE0MTEsImV4cCI6MjEwMDExNzQxMX0.5YLtkFTon-5Qkm_hIBwcQb-0JZFCTyNw1QnlAzerxAg";
 
 const db = window.supabase.createClient(
     SUPABASE_URL,
@@ -16,3 +16,5 @@ const db = window.supabase.createClient(
 );
 
 window.db = db;
+
+console.log("✅ Supabase Connected");
