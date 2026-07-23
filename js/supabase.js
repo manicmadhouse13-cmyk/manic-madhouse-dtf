6a8e04941f839873d54ed464d9a1e8e689a34ca4
manic-madhouse-dtf <manicmadhouse13@gmail.com>
2026-07-23 18:04:53 +1000

    Refactor Supabase client initialization

--- js/supabase.js
+++ js/supabase.js
@@ -4,21 +4,17 @@ SUPABASE CONNECTION
 VERSION 2.0
 ==================================================*/
 
-const SUPABASE_URL =
-"https://ymkmpsgossabyznwhluk.supabase.co";
+const SUPABASE_URL = "https://ymkmpsgossabyznwhluk.supabase.co";
 
-const SUPABASE_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlta21wc2dvc3NhYnl6bndobHVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NDE0MTEsImV4cCI6MjEwMDExNzQxMX0.5YLtkFTon-5Qkm_hIBwcQb-0JZFCTyNw1QnlAzerxAg";
+const SUPABASE_KEY = 
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlta21wc2dvc3NhYnl6bndobHVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NDE0MTEsImV4cCI6MjEwMDExNzQxMX0.5YLtkFTon-5Qkm_hIBwcQb-0JZFCTyNw1QnlAzerxAg";;
 
-
-
-const supabase =
-window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
-console.log("window.supabase =", window.supabase);
-console.log("supabase =", supabase);
-console.log("typeof supabase.from =", typeof supabase.from);
 
-console.log("✅ Supabase Connected");
+window.db = supabaseClient;
+
+console.log("Supabase client:", window.db);
+console.log("typeof window.db.from =", typeof window.db.from);
