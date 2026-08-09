dcae63501781ebe676af06a4045db53c70da5aeb
manic-madhouse-dtf <manicmadhouse13@gmail.com>
2026-08-10 05:41:11 +1000

    Change Supabase key to placeholder text
    Updated Supabase key placeholder for security.

--- js/supabase.js
+++ js/supabase.js
@@ -5,12 +5,16 @@ SUPABASE CONNECTION
 VERSION 2.0
 ==================================================*/
 
+<script src="js/supabase.js?v=2"></script>alert("SUPABASE FILE LOADED");
+
+alert("SUPABASE LIBRARY: " + (window.supabase !== undefined));
+
 const SUPABASE_URL =
 "https://sbrixpwtvhaundmtilpa.supabase.co";
 
 
 const SUPABASE_KEY =
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicml4cHd0dmhhdW5kbXRpbHBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxNzI5NDcsImV4cCI6MjEwMTc
+"PASTE YOUR FULL ANON KEY HERE";
 
 
 const db = window.supabase.createClient(
