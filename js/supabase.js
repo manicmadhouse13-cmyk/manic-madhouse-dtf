9cea31576006ae1bfddb10ac3da522997d97189c
manic-madhouse-dtf <manicmadhouse13@gmail.com>
2026-08-09 20:59:10 +1000

    Update Supabase URL and key placeholder
    Updated Supabase URL and replaced key with a placeholder.

--- js/supabase.js
+++ js/supabase.js
@@ -5,10 +5,13 @@ VERSION 2.0
 ==================================================*/
 
 const SUPABASE_URL =
-https://sbrixpwtvhaundmtilpa.supabase.co/rest/v1/
+"https://sbrixpwtvhaundmtilpa.supabase.co";
+
 
 const SUPABASE_KEY =
-    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicml4cHd0dmhhdW5kbXRpbHBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxNzI5NDcsImV4cCI6MjEwMTc0ODk0N30.aEjr_H2pGuoSTb0rRPHS4uaFlUq9IFQmt_KxLQ5fKmw
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....your full key here";
+
+
 const db = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
