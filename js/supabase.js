1070d6e8b3288efefc919d13079d547aefb27f6e
manic-madhouse-dtf <manicmadhouse13@gmail.com>
2026-08-09 21:03:01 +1000

    Change Supabase key to new value
    Updated Supabase key to a new value.

--- js/supabase.js
+++ js/supabase.js
@@ -9,7 +9,7 @@ const SUPABASE_URL =
 
 
 const SUPABASE_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....your full key here";
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicml4cHd0dmhhdW5kbXRpbHBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxNzI5NDcsImV4cCI6MjEwMTc
 
 
 const db = window.supabase.createClient(
