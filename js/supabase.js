088e9df54f42c90ee00106c05b2b0b50f25b23da
manic-madhouse-dtf <manicmadhouse13@gmail.com>
2026-08-10 17:10:47 +1000

    Update Supabase key in supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -10,7 +10,7 @@ const SUPABASE_URL =
 
 
 const SUPABASE_KEY =
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicml4cHd0dmhhdW5kbXRpbHBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxNzI5NDcsImV4cCI6MjEwMTc
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicml4cHd0dmhhdW5kbXRpbHBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxNzI5NDcsImV4cCI6MjEwMTc"
 
 
 window.db = window.supabase.createClient(
