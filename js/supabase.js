f96037db1a3ce598856197276556279f1199b834
manic-madhouse-dtf <manicmadhouse13@gmail.com>
2026-08-25 14:30:59 +1000

    Refactor Supabase client initialization
    Removed alert and console logs, streamlined Supabase client initialization.

--- js/supabase.js
+++ js/supabase.js
@@ -1,31 +1,17 @@
-alert("SUPABASE FILE LOADED");
-/*==================================================
+/*=========================================
 MANIC MADHOUSE DTF DESIGNS
 SUPABASE CONNECTION
-==================================================*/
+=========================================*/
+
+const SUPABASE_URL = "https://ymkmpsgossabyznwhluk.supabase.co";
+
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlta21wc2dvc3NhYnl6bndobHVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NDE0MTEsImV4cCI6MjEwMDExNzQxMX0.5YLtkFTon-5Qkm_hIBwcQb-0JZFCTyNw1QnlAzerxAg";
 
 
-const SUPABASE_URL =
-"https://ymkmpsgossabyznwhluk.supabase.co";
-
-
-const SUPABASE_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlta21wc2dvc3NhYnl6bndobHVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NDE0MTEsImV4cCI6MjEwMDExNzQxMX0.5YLtkFTon-5Qkm_hIBwcQb-0JZFCTyNw1QnlAzerxAg";
-
-console.log(
-"URL:",
-SUPABASE_URL
-);
-
-console.log(
-"KEY START:",
-SUPABASE_KEY.substring(0,10)
-);
-window.db =
-window.supabase.createClient(
-SUPABASE_URL,
-SUPABASE_KEY
+window.supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
 );
 
 
-console.log("✅ Supabase Connected");
+console.log("Supabase Client Ready");
